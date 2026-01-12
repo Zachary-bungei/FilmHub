d2dc3de822b0b1ab217ac270a9a924b51bb1a117
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2026-01-12 16:37:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
 
   if (req.method === 'OPTIONS') {
